73f41414d7bdcba012fb830d0a288ce22cfd1b59
junewoop <40329273+junewoop@users.noreply.github.com>
2020-10-23 16:23:50 -0400

    Revert "Update FilterBlur.cpp"
    This reverts commit 6e15126f25ad48c148176c9ce82bfd1932ed9788.

--- filter/FilterBlur.cpp
+++ filter/FilterBlur.cpp
@@ -22,7 +22,7 @@ void FilterBlur::apply(Canvas2D *canvas){
     std::vector<float> vec_r, vec_g, vec_b;
     loadImage(canvas, vec_r, vec_g, vec_b);
 
-    // 1D horizontal kernel
+    //TODO: 1D horizontal kernel
     std::vector<float> tmp_r, tmp_g, tmp_b;
     tmp_r.resize(m_width*m_height);
     tmp_g.resize(m_width*m_height);
@@ -30,16 +30,16 @@ void FilterBlur::apply(Canvas2D *canvas){
     for (int r = 0; r < m_height; r++)
         blur1D(r*m_width, vec_r, vec_g, vec_b, tmp_r, tmp_g, tmp_b, X_Axis);
 
-    // 1D vertical kernel
+    //TODO: 1D vertical kernel
     std::vector<float> final_r, final_g, final_b;
     final_r.resize(m_width*m_height);
     final_g.resize(m_width*m_height);
     final_b.resize(m_width*m_height);
-    for (int c = 0; c < m_width; c++)
-        blur1D(c, tmp_r, tmp_g, tmp_b, final_r, final_g, final_b, Y_Axis);
+    //for (int c = 0; c < m_width; c++)
+    //    blur1D(c, tmp_r, tmp_g, tmp_b, final_r, final_g, final_b, Y_Axis);
 
     //save Image
-    saveImage(canvas, final_r, final_g, final_b);
+    saveImage(canvas, tmp_r, tmp_g, tmp_b);
 }
 
 void FilterBlur::blur1D(int beginIndex,
@@ -56,11 +56,10 @@ void FilterBlur::blur1D(int beginIndex,
         num_step = m_height;
     }
 
-    // compute the first pixel
-    next_r[beginIndex] = m_r * vec_r[beginIndex]*m_ker;
-    next_g[beginIndex] = m_r * vec_g[beginIndex]*m_ker;
-    next_b[beginIndex] = m_r * vec_b[beginIndex]*m_ker;
     int endIndex = shiftIndex(beginIndex, num_step-1, m_width, axis);
+    next_r[beginIndex] = m_r * vec_r[beginIndex] * m_ker;
+    next_g[beginIndex] = m_r * vec_g[beginIndex] * m_ker;
+    next_b[beginIndex] = m_r * vec_b[beginIndex] * m_ker;
     for (int j = 0; j <= m_r; j++){
         if(j < num_step){
             ind = shiftIndex(beginIndex, j, m_width, axis);
@@ -75,7 +74,6 @@ void FilterBlur::blur1D(int beginIndex,
         }
     }
 
-    // compute the rest of the pixels
     int prev_ind, tmp_ind;
     for (int j = 1; j < num_step; j++){
         prev_ind = shiftIndex(beginIndex, j-1, m_width, axis);
@@ -92,15 +90,17 @@ void FilterBlur::blur1D(int beginIndex,
             next_b[ind] = next_b[prev_ind] - vec_b[tmp_ind]*m_ker;
         }
         if(j+m_r >= num_step){
-            next_r[ind] += vec_r[endIndex]*m_ker;
-            next_g[ind] += vec_g[endIndex]*m_ker;
-            next_b[ind] += vec_b[endIndex]*m_ker;
+            next_r[ind] = next_r[prev_ind] + vec_r[endIndex]*m_ker;
+            next_g[ind] = next_g[prev_ind] + vec_g[endIndex]*m_ker;
+            next_b[ind] = next_b[prev_ind] + vec_b[endIndex]*m_ker;
         }
         else{
             tmp_ind = shiftIndex(beginIndex, j+m_r, m_width, axis);
-            next_r[ind] += vec_r[tmp_ind]*m_ker;
-            next_g[ind] += vec_g[tmp_ind]*m_ker;
-            next_b[ind] += vec_b[tmp_ind]*m_ker;
+            next_r[ind] = next_r[prev_ind] + vec_r[tmp_ind]*m_ker;
+            next_g[ind] = next_g[prev_ind] + vec_g[tmp_ind]*m_ker;
+            next_b[ind] = next_b[prev_ind] + vec_b[tmp_ind]*m_ker;
         }
     }
 }
+
+

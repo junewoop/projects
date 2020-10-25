848ea9f1635f2ee8af903c13ea4b20842eafb138
junewoop <40329273+junewoop@users.noreply.github.com>
2020-10-25 15:12:38 -0400

    Revert "lab 6"
    This reverts commit 8a8cd6aa610b2e6a07aebe18ec5cfc29dc1c8627.

--- camera/CamtransCamera.cpp
+++ camera/CamtransCamera.cpp
@@ -8,172 +8,94 @@
 #include "CamtransCamera.h"
 #include <Settings.h>
 
-CamtransCamera::CamtransCamera() :
-    m_aspectRatio(0.f),
-    m_near(0.f),
-    m_far(0.f),
-    m_translationMatrix(glm::mat4x4()),
-    m_perspectiveTransformation(glm::mat4x4()),
-    m_scaleMatrix(glm::mat4x4()),
-    m_rotationMatrix(glm::mat4x4()),
-    m_thetaH(0.f),
-    m_eye(glm::vec4()),
-    m_up(glm::vec4()),
-    m_u(glm::vec4()),
-    m_v(glm::vec4()),
-    m_w(glm::vec4())
+CamtransCamera::CamtransCamera()
 {
     // @TODO: [CAMTRANS] Fill this in...
-    setClip(1, 30);
-    setHeightAngle(glm::radians(60.f));
-    setAspectRatio(1.f);
-    orientLook(glm::vec4(2, 2, 2, 1),
-               glm::vec4(-2, -2, -2, 1),
-               glm::vec4(0, 1, 0, 1));
 }
 
 void CamtransCamera::setAspectRatio(float a)
 {
     // @TODO: [CAMTRANS] Fill this in...
-    // Question
-    m_aspectRatio = a;
-    updateProjectionMatrix();
+
 }
 
 glm::mat4x4 CamtransCamera::getProjectionMatrix() const {
     // @TODO: [CAMTRANS] Fill this in...
-    return m_perspectiveTransformation*m_scaleMatrix;
+    return glm::mat4x4();
 }
 
 glm::mat4x4 CamtransCamera::getViewMatrix() const {
-    return m_rotationMatrix*m_translationMatrix;
+    // @TODO: [CAMTRANS] Fill this in...
+    return glm::mat4x4();
 }
 
 glm::mat4x4 CamtransCamera::getScaleMatrix() const {
-    return m_scaleMatrix;
+    // @TODO: [CAMTRANS] Fill this in...
+    return glm::mat4x4();
 }
 
 glm::mat4x4 CamtransCamera::getPerspectiveMatrix() const {
-    return m_perspectiveTransformation;
+    // @TODO: [CAMTRANS] Fill this in...
+    return glm::mat4x4();
 }
 
 glm::vec4 CamtransCamera::getPosition() const {
-    return m_eye;
+    // @TODO: [CAMTRANS] Fill this in...
+    return glm::vec4();
 }
 
 glm::vec4 CamtransCamera::getLook() const {
-    return -m_w;
+    // @TODO: [CAMTRANS] Fill this in...
+    return glm::vec4();
 }
 
 glm::vec4 CamtransCamera::getUp() const {
-    return m_up;
-}
-
-glm::vec4 CamtransCamera::getU() const {
-    return m_u;
-}
-
-glm::vec4 CamtransCamera::getV() const {
-    return m_v;
-}
-
-glm::vec4 CamtransCamera::getW() const {
-    return m_w;
+    // @TODO: [CAMTRANS] Fill this in...
+    return glm::vec4();
 }
 
 float CamtransCamera::getAspectRatio() const {
-    return m_aspectRatio;
+    // @TODO: [CAMTRANS] Fill this in...
+    return 0;
 }
 
 float CamtransCamera::getHeightAngle() const {
-    return m_thetaH;
+    // @TODO: [CAMTRANS] Fill this in...
+    return 0;
 }
 
 void CamtransCamera::orientLook(const glm::vec4 &eye, const glm::vec4 &look, const glm::vec4 &up) {
     // @TODO: [CAMTRANS] Fill this in...
-    m_eye = eye;
-    m_up = up;
-    m_w = glm::normalize(-look);
-    m_v = glm::normalize(up-glm::dot(m_up, m_w)*m_w);
-    // Question!
-    m_u = glm::vec4(glm::cross(glm::vec3(m_u.xyz()), glm::vec3(m_w.xyz())), 1);
-    updateViewMatrix();
-    updateProjectionMatrix();
+
 }
 
 void CamtransCamera::setHeightAngle(float h) {
-    m_thetaH = h;
-    updateProjectionMatrix();
+    // @TODO: [CAMTRANS] Fill this in...
+
 }
 
 void CamtransCamera::translate(const glm::vec4 &v) {
-    m_eye += v;
-    updateViewMatrix();
+    // @TODO: [CAMTRANS] Fill this in...
+
 }
 
 void CamtransCamera::rotateU(float degrees) {
-    float rad = glm::radians(degrees);
-    glm::vec4 tmp = cos(rad)*m_v + sin(rad)*m_w;
-    m_w = -sin(rad)*m_v + cos(rad)*m_w;
-    m_v = tmp;
+    // @TODO: [CAMTRANS] Fill this in...
+
 }
 
 void CamtransCamera::rotateV(float degrees) {
-    float rad = glm::radians(degrees);
-    glm::vec4 tmp = cos(rad)*m_w + sin(rad)*m_u;
-    m_u = -sin(rad)*m_w + cos(rad)*m_u;
-    m_w = tmp;
+    // @TODO: [CAMTRANS] Fill this in...
+
 }
 
 void CamtransCamera::rotateW(float degrees) {
-    float rad = glm::radians(degrees);
-    glm::vec4 tmp = cos(rad)*m_u + sin(rad)*m_v;
-    m_v = -sin(rad)*m_u + cos(rad)*m_v;
-    m_u = tmp;
+    // @TODO: [CAMTRANS] Fill this in...
+
 }
 
 void CamtransCamera::setClip(float nearPlane, float farPlane) {
-    m_near = nearPlane;
-    m_far = farPlane;
-}
+    // @TODO: [CAMTRANS] Fill this in...
 
-void CamtransCamera::updateProjectionMatrix(){
-    updateScaleMatrix();
-    updatePerspectivematrix();
-}
-
-void CamtransCamera::updateViewMatrix(){
-    updateTranslationMatrix();
-    updateRotationMatrix();
-}
-
-void CamtransCamera::updatePerspectivematrix(){
-    float c= -m_near/m_far;
-    m_perspectiveTransformation = glm::mat4x4(1, 0, 0, 0,
-                                              0, 1, 0, 0,
-                                              0, 0, -1/(c+1), -1,
-                                              0, 0, c/(c+1), 0);
-}
-
-void CamtransCamera::updateScaleMatrix(){
-    m_scaleMatrix = glm::mat4x4(1/(tan(m_thetaH/2)*m_aspectRatio*m_far), 0, 0, 0,
-                                0, 1/(tan(m_thetaH/2)*m_far), 0, 0,
-                                0, 0, 1/m_far, 0,
-                                0, 0, 0, 1);
-}
-
-void CamtransCamera::updateRotationMatrix(){
-    m_rotationMatrix = glm::mat4x4(m_u.x, m_v.x, m_w.x, 0,
-                                   m_u.y, m_v.y, m_w.y, 0,
-                                   m_u.z, m_v.z, m_w.z, 0,
-                                   0, 0, 0, 1);
-
-}
-
-void CamtransCamera::updateTranslationMatrix(){
-    m_translationMatrix = glm::mat4x4(1, 0, 0, 0,
-                                      0, 1, 0, 0,
-                                      0, 0, 1, 0,
-                                      -m_eye.x, -m_eye.y, -m_eye.z, 1);
 }
 
